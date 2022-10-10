86a7a1bc6b365d6bc7043b86545dfd481cd14c4a
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-10 11:43:34 +0200

    Exection file done

---
+++ execution.rb
@@ -0,0 +1,15 @@
+require './book/create_book'
+require './label/create_label'
+require './book/list_books'
+
+class Execution
+    include AddLabel
+    include AddBook
+    include ListBook
+
+    def initialize
+        @books = []
+        @labels = []
+    end
+
+end
