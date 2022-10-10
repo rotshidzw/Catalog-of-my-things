34ef830dbb84068d7d60d4352aca01f08f6d04b2
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-10 16:09:02 +0200

    Fix errors

--- execution.rb
+++ execution.rb
@@ -1,11 +1,15 @@
 require './book/create_book'
 require './label/create_label'
 require './book/list_books'
+require './storage_methods/save_all_data'
+require './storage_methods/load_data'
 
 class Execution
   include AddLabel
   include AddBook
   include ListBook
+  include LoadData
+  include SaveAllData
 
   def initialize
     @books = []
