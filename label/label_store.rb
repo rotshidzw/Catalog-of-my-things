4f95824587c6c1b1299cf57dbf5234905fbf7ee9
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-10 11:30:24 +0200

    Add label and create schema

---
+++ label/label_store.rb
@@ -0,0 +1,27 @@
+require 'json'
+require './label'
+
+# the book module
+module LabelStore
+  def load_labels
+    label_store = []
+    label_file = open('./labels.json')
+    if File.exist?(book_file) && File.read(book_file) != ''
+      data = label_file.read
+      JSON.parse(data).each do |book|
+        label_store << Book.new(book['id'], book['publisher'], book['cover_state'])
+      end
+    else
+      File.write(label_file, '[]')
+    end
+    label_store
+  end
+
+  def save_books_to_file
+    label_store = []
+    @labels.each do |label|
+      label_store << { id: label.id, title: label.title, color: label.color }
+    end
+    File.write('./labels.json', label_store.to_json)
+  end
+end
---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE catalog_things_db;
+
+CREATE TABLE books (
+    id INT GENERATED ALWAYS AS IDENTITY
+    genre_id
+    author_id
+    label_id 
+    publish_date DATE
+    archived BOOLEAN
+    publisher VARCHAR(190)
+    cover_state VARCHAR(190)
+
+    CONSTRAINT fk_books_label_id FOREIGN KEY (label_id) REFERENCES labels(id)
+
+    CONSTRAINT fk_books_author_id FOREIGN KEY (author_id) REFERENCES authors(id)
+
+    CONSTRAINT fk_books_genre_id FOREIGN KEY (genre_id) REFERENCES genres(id)
+
+    PRIMARY KEY (id)
+);
