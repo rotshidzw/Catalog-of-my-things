94d1ee15f8268c4c99e518b07311dd83668e9b70
piesieomane <piesieyeboah@gmail.com>
2022-10-11 17:19:49 +0000

    Add methods and constructor for game class

--- game.rb
+++ game.rb
@@ -1,4 +1,17 @@
 require_relative 'item'
 
 class Game < item
+    def initialize(id, publish_date, multiplayer, last_played_at)
+        super(id, publish_date)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    def can_be_archived?
+        if @last_played_at < 2.years.ago
+            return true
+        else
+            return false
+        end
+    end
 end
