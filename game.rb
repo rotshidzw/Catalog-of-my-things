48d9b35b405051efe3b1fd66f2ee722630e4acf6
piesieomane <piesieyeboah@gmail.com>
2022-10-12 09:02:37 +0000

    Add remaining fixes

--- game.rb
+++ game.rb
@@ -3,6 +3,8 @@
 require_relative 'item'
 
 class Game < item
+  attr_accessor :multiplayer, :last_played_at
+
   def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
@@ -10,6 +12,6 @@ class Game < item
   end
 
   def can_be_archived?
-    @last_played_at < 2.years.ago
+    super && @last_played_at < 2.years.ago
   end
 end
