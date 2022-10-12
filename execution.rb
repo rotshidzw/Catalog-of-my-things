758b9e6b71ec89dbbf9c9bd3573818609c889654
Abdullah2213565 <dulakhan024@gmail.com>
2022-10-12 13:43:23 +0200

    Fix some errors

--- execution.rb
+++ execution.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './book/create_book'
 require './label/create_label'
 require './book/list_books'
--- spec/label.spec.test.rb
+++ spec/label.spec.test.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './label/label'
 require './book/book'
 describe Label do
